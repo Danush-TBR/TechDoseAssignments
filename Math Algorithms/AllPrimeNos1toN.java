ec7aff5be3fbec758fb92afb5cd922c25e33d9c9
Danush-TBR <77879683+Danush-TBR@users.noreply.github.com>
2021-12-09 10:05:49 +0530

    Create AllPrimeNos1toN.java

---
+++ Math Algorithms/AllPrimeNos1toN.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class AllPrimeNos1toN {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int n=scanner.nextInt();
+        boolean[] primes = new boolean[n];
+        for(int i=2;i<=n;i++){
+            if(primes[i-1]==false){
+                for(int j=2;j*i<=n;j++) primes[(i*j)-1]=true;
+            }
+        }
+        for(int i=1;i<n;i++){
+            if(primes[i]==false) System.out.print(i+1+" ");
+        }
+    }
+}
