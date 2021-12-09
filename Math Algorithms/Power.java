5009f97f5db46c7c0e37fbd4e715f2aeeb4c08f4
Danush-TBR <77879683+Danush-TBR@users.noreply.github.com>
2021-12-09 12:14:22 +0530

    Update Power.java

--- Math Algorithms/Power.java
+++ Math Algorithms/Power.java
@@ -6,7 +6,7 @@ public class Power
         int res = 1;
         while (y > 0)
         {
-            if ((y & 1) == 1) res = res * x;
+            if (y & 1) res = res * x;
             y = y/2;
             x = x*x;
         }
