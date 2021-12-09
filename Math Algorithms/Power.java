8f7fb34dd51f87b8fe84ff7676f01a80bf5e3b59
Danush-TBR <77879683+Danush-TBR@users.noreply.github.com>
2021-12-09 12:13:51 +0530

    Create Power.java

---
+++ Math Algorithms/Power.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Power
+{
+    static int power(int x, int y)
+    {
+        int res = 1;
+        while (y > 0)
+        {
+            if ((y & 1) == 1) res = res * x;
+            y = y/2;
+            x = x*x;
+        }
+        return res;
+    }
+
+public static void main (String[] args)
+{
+    Scanner scanner = new Scanner(System.in);
+    int x = scanner.nextInt();
+    int y = scanner.nextInt();
+ 
+    System.out.println("Power is " +power(x, y));
+}
+}
