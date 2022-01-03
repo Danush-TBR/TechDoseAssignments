4245a9f5902653d96ff20fc8e7cbe1a68d55d0bb
Danush-TBR <77879683+Danush-TBR@users.noreply.github.com>
2022-01-03 22:46:23 +0530

    Update previous Smaller element.java

--- 6.Stack & Queue/previous Smaller element.java
+++ 6.Stack & Queue/previous Smaller element.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class nextSmallElement {
+public class previousSmallElement {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n=scanner.nextInt(); int[] arr = new int[n];
