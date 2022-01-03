3e00f978d835ccf76af353e21df1b3d045a784b8
Danush-TBR <77879683+Danush-TBR@users.noreply.github.com>
2022-01-03 22:44:47 +0530

    Create next Smaller Element.java

---
+++ 6.Stack & Queue/next Smaller Element.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class nextSmallElement {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n=scanner.nextInt(); int[] arr = new int[n];
+        for(int i=0;i<n;i++) arr[i]=scanner.nextInt();
+        Stack<Integer> myStack = new Stack<Integer>();
+        for(int i=0;i<n;i++){
+            if(myStack.empty()) myStack.push(i);
+            else if(arr[myStack.peek()]<=arr[i]) myStack.push(i);
+            else{
+                while(!myStack.empty() && arr[myStack.peek()]>arr[i]){
+                    arr[myStack.pop()]=arr[i];
+                }
+                myStack.push(i);
+            }
+        }
+        while(!myStack.empty()) arr[myStack.pop()]=-1;
+        for(int i=0;i<n;i++) System.out.print(arr[i]+" ");
+        scanner.close();
+    }
+}
