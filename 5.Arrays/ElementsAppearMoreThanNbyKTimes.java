ee992c90ea9077642303064c443f216240d33958
Danush-TBR <77879683+Danush-TBR@users.noreply.github.com>
2022-02-24 11:30:30 +0530

    Create ElementsAppearMoreThanNbyKTimes.java

---
+++ 5.Arrays/ElementsAppearMoreThanNbyKTimes.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<Integer> majorityElement(int[] nums,int k) {
+        int len=nums.length; int K=len/k;
+        Map<Integer,Integer> myMap = new LinkedHashMap<Integer,Integer>();
+        List myList = new ArrayList<Integer>();
+        for(int i:nums){
+            myMap.putIfAbsent(i,0);
+            myMap.put(i,myMap.get(i)+1);
+        }
+        for(Map.Entry<Integer,Integer> entry: myMap.entrySet()){
+            if(entry.getValue()>K){
+                myList.add(entry.getKey());
+            }
+        }
+        return myList;
+    }
+}
