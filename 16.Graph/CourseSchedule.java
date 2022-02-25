f4349e56bc84161465c3a92b8419ca0846bef88d
Danush-TBR <77879683+Danush-TBR@users.noreply.github.com>
2022-02-25 19:03:32 +0530

    Create CourseSchedule.java

---
+++ 16.Graph/CourseSchedule.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isCyclic(List<List<Integer>> prerequsite,int curr,int[] visited){
+        if(visited[curr]==1) return true;
+        visited[curr]=1;
+        for(int i:prerequsite.get(curr)){
+            if(visited[i]!=2)
+                if(isCyclic(prerequsite,i,visited)) return true;
+        }
+        visited[curr]=2;
+        return false;
+    }
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int plen = prerequisites.length;
+        List<List<Integer>> prerequsite = new ArrayList<>();
+        for(int i=0;i<numCourses;i++) prerequsite.add(new ArrayList<>());
+        for(int i=0;i<plen;i++) prerequsite.get(prerequisites[i][0]).add(prerequisites[i][1]);
+        int[] visited = new int[numCourses];
+        for(int i=0;i<numCourses;i++){
+            if(isCyclic(prerequsite,i,visited)) return false;
+        }
+        return true;
+    }
+}
