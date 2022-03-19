2a9e3f8c165c9df32bd478221746e8dea0aae9d1
Danush <psdanush6900@gmail.com>
2022-03-19 23:38:38 +0530

    largest subarray sum less than K

--- 5.Arrays/largestSubarraySumLessThanN.java
+++ 5.Arrays/largestSubarraySumLessThanN.java
@@ -7,6 +7,32 @@ public class largestSubarraySumLessThanN {
         for(int i=0;i<N;i++){
             arr[i]=scanner.nextInt();
         }
+        int K=scanner.nextInt();
+        int l=0,r=0,currSum=0;
+        while(r<N){
+            // System.out.println(l+" "+r);
+            if(currSum<K){
+                currSum+=arr[r];
+                if(currSum<=K){
+                    // System.out.println(l+" "+r+" "+currSum);
+                    max_len=Math.max(r-l+1,max_len);
+                }
+                r++;
+            }
+            else if(arr[r]>K){
+                l=r+1;
+                r=r+1;
+                currSum=0;
+            }
+            else{
+                currSum-=arr[l];
+                l++;
+                if(l==r){
+                    l++; r++;
+                }
+            }
+        }
+        System.out.println(max_len);
         scanner.close();
     }
 }
